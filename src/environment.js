133e8685a1e523975b29829cd7150907800df801
Hans Schuell <hans.schuell@gmx.de>
2022-07-08 20:51:55 +0200

    enviroment.js introduced

---
+++ src/environment.js
@@ -0,0 +1,11 @@
+// Open Source Processor - see https://github.com/giraone/thymeleaf-processor
+export let processJsonToHtmlUrl = 'http://localhost:8080/api/json-to-html';
+export let processJsonToPdfUrl = 'http://localhost:8080/api/json-to-pdf';
+
+// ReportRenderingService in DEV master
+// export let processJsonToHtmlUrl = 'https://report-rendering-rechtsform-api.pcfsec.dev.datev.de/api/v1/multipart-to-html';
+// export let processJsonToPdfUrl = 'https://report-rendering-rechtsform-api.pcfsec.dev.datev.de/api/v1/multipart-to-pdf';
+
+// Local ReportRenderingService
+// export let processJsonToHtmlUrl = 'http://localhost:8083/api/v1/multipart-to-html';
+// export let processJsonToPdfUrl = 'http://localhost:8083/api/v1/multipart-to-pdf';
