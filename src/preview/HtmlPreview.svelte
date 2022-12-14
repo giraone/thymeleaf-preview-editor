5081a962d1285ee1695670daf86c6d58a9a87ea2
Hans Schuell <hans.schuell@gmx.de>
2022-12-14 20:13:54 +0100

    Issue #1 fixed: Scaler for HTML preview

--- src/preview/HtmlPreview.svelte
+++ src/preview/HtmlPreview.svelte
@@ -2,6 +2,20 @@
   // The id of the iframe
   export let id = Math.floor(Math.random() * 1000000);
 
+  function changeScale() {
+      const scaler = document.getElementById('os');
+      if (!scaler) {
+        return;
+      }    
+      var s = parseFloat(scaler.value);
+      const frame = document.getElementById(id);
+      if (!frame) {
+        return;
+      }    
+      frame.style.transform='scale(' + s + ')';
+      document.getElementById('scale').innerHTML = s.toFixed(2);
+  }
+
   //-- Exported functions ------------------------------------------------------------
 
   export const htmlPreview = {
@@ -10,18 +24,18 @@
       iframe.srcdoc = htmlContent;
     }
   };
-
 </script>
 
 <!-- HTML ------------------------------------------------------------------------ -->
 
+<div class="scale">
+<input id="os" class="scale" type="range" min="0.25" max="8.0" value="1.0" step="0.25" on:change="{changeScale}" />
+<span class="scale" id="scale">1.00</span>
+</div>
 <div class="preview">
-  <iframe id="{id}" class="preview" title="HTML Preview"
-    srcdoc="&lt;pre&gt;No HTML preview yet! Lookup in the window below for errors.&lt;/pre&gt;"
-  ></iframe>
+  <iframe id="{id}" class="preview" title="HTML Preview" srcdoc="&lt;pre&gt;No HTML preview yet! Lookup in the window below for errors.&lt;/pre&gt;"></iframe>
 </div>
 
 <!-- CSS ------------------------------------------------------------------------- -->
-
 <style>
 </style>
